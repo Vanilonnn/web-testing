60fd9ab0669155c794bd1fa1c1359b750cb515cc
Vanilonnn <104099919+Vanilonnn@users.noreply.github.com>
2025-03-03 17:43:29 +0500

    init+

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for djangoProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from accounts import views
+from accounts.views import forum_home, create_topic, topic_detail, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('', home, name='home'),
+    path('forum/', forum_home, name='forum_home'),
+    path('forum/topic/new/', create_topic, name='create_topic'),
+    path('forum/topic/<int:topic_id>/', topic_detail, name='topic_detail'),
 ]
